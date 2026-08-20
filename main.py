ac5e708122b471180af4f680b25dd06f742d3fbd
HubTakenIs <hubertaslol@gmail.com>
2026-08-20 22:02:35 +0100

    found new terminal menu to use in the project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import calendar, datetime
 from db import get_db_conn
+from simple_term_menu import TerminalMenu
 
 def silly_func():
     out = []
@@ -25,15 +26,21 @@ def list_days_for_month(year, month):
     return days
 
 def main():
-   list_of_dates = list_dates_for_month(2026, calendar.JANUARY)
-   print(list_of_dates)
-   print("days?")
-   list_of_days = list_days_for_month(2026, calendar.JANUARY)
-   print(list(list_of_days))
-   c = calendar.Calendar()
-   jan_weeks = c.monthdayscalendar(2026, calendar.JANUARY)
-   print("jan")
-   print(jan_weeks)
+   #list_of_dates = list_dates_for_month(2026, calendar.JANUARY)
+   #print(list_of_dates)
+   #print("days?")
+   #list_of_days = list_days_for_month(2026, calendar.JANUARY)
+   #print(list(list_of_days))
+   #c = calendar.Calendar()
+   #jan_weeks = c.monthdayscalendar(2026, calendar.JANUARY)
+   #print("jan")
+   #print(jan_weeks)
+   #!/usr/bin/env python3
+    options = ["entry 1", "entry 2", "entry 3"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(f"You have selected {options[menu_entry_index]}!")
+   
    
 
 if __name__ == "__main__":
